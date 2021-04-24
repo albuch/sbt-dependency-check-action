dbc4d07dc3ba4ff786284009b9db062b9d55cbc2
Alexander v. Buchholtz <albuch@users.noreply.github.com>
2021-04-24 13:44:03 +0200

    :sparkles: Printing found vulnerabilities and failing build id any vulnerability found

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,26 @@
-#!/bin/sh -l
+#!/bin/bash
+
+set -e
 
 json_path=$1
 
-echo "Checking JSON report..."
+echo -n "Checking JSON report: "
 
 if [ -f "$json_path" ]; then
-  cat "$json_path";
+  declare -a vulnerabilities
+  readarray -t vulnerabilities < <( jq -c '.dependencies[] | select(.vulnerabilities | length > 0) | {filename: .fileName, vulnerabilities: ([.vulnerabilities[].name] | join(", ")), maxCvssv3Score: (.vulnerabilities | max_by(.cvssv3.baseScore) | .cvssv3.baseScore )}' "$json_path")
+  if [ ${#vulnerabilities[@]} -gt 0 ]; then
+      printf "Found %d vulnerabilities!\n\n" ${#vulnerabilities[@]}
+
+      for vulnerability in "${vulnerabilities[@]}"; do
+        printf "%s\n\n" "$vulnerability"
+      done
+      echo "Failing pipeline."
+      exit 1
+  else
+    echo "No vulnerabilities found."
+  fi
 else
-  printf "'%s' not found" "${json_path}"
-  ls -R target/
+  echo "Error: JSON report path '${json_path}' not found"
+  exit 1
 fi
